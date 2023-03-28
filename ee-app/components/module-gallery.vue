902de4a6a198d5fd382a7eeda1432d04b4738b43
Spectrum3900 <jeromeheuze@gmail.com>
2023-03-28 15:27:05 -0700

    up

--- ee-app/components/module-gallery.vue
+++ ee-app/components/module-gallery.vue
@@ -18,7 +18,7 @@
 
 <script>
 import CoolLightBox from "vue-cool-lightbox";
-import "vue-cool-lightbox/dist/vue-cool-lightbox.min.css";
+//import "vue-cool-lightbox/dist/vue-cool-lightbox.min.css";
 export default {
   props: ['items', 'index'],
   components: { CoolLightBox }
