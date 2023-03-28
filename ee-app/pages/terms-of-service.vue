2a7834ecade837397a2e0541b8f6c56a93f85302
Spectrum3900 <jeromeheuze@gmail.com>
2023-03-28 15:50:51 -0700

    up

--- ee-app/pages/terms-of-service.vue
+++ ee-app/pages/terms-of-service.vue
@@ -8,7 +8,7 @@
         <div class="breadcrumbs">
           <a href="/">Home</a> <em class="bc-next"></em> <span>Website Terms and Conditions of Use</span>
         </div>
-        <h1>Website Terms and Conditions of Use for EntropiaMarket</h1>
+        <h1>Website Terms and Conditions of Use for EntropiaEstate</h1>
         <section class="textContent">
 
           <div class="row">
@@ -20,37 +20,37 @@
 
               <h2>2. Use License</h2>
 
-              <p>Permission is granted to temporarily download one copy of the materials on EntropiaMarket's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+              <p>Permission is granted to temporarily download one copy of the materials on EntropiaEstate's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
 
               <ul>
                 <li>modify or copy the materials;</li>
                 <li>use the materials for any commercial purpose or for any public display;</li>
-                <li>attempt to reverse engineer any software contained on EntropiaMarket's Website;</li>
+                <li>attempt to reverse engineer any software contained on EntropiaEstate's Website;</li>
                 <li>remove any copyright or other proprietary notations from the materials; or</li>
                 <li>transferring the materials to another person or "mirror" the materials on any other server.</li>
               </ul>
 
-              <p>This will let EntropiaMarket to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format. These Terms of Service has been created with the help of the <a href="https://www.termsofservicegenerator.net">Terms Of Service Generator</a>.</p>
+              <p>This will let EntropiaEstate to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format. These Terms of Service has been created with the help of the <a href="https://www.termsofservicegenerator.net">Terms Of Service Generator</a>.</p>
 
               <h2>3. Disclaimer</h2>
 
-              <p>All the materials on EntropiaMarket’s Website are provided "as is". EntropiaMarket makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, EntropiaMarket does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
+              <p>All the materials on EntropiaEstate’s Website are provided "as is". EntropiaEstate makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, EntropiaEstate does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
 
               <h2>4. Limitations</h2>
 
-              <p>EntropiaMarket or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on EntropiaMarket’s Website, even if EntropiaMarket or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
+              <p>EntropiaEstate or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on EntropiaEstate’s Website, even if EntropiaEstate or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
 
               <h2>5. Revisions and Errata</h2>
 
-              <p>The materials appearing on EntropiaMarket’s Website may include technical, typographical, or photographic errors. EntropiaMarket will not promise that any of the materials in this Website are accurate, complete, or current. EntropiaMarket may change the materials contained on its Website at any time without notice. EntropiaMarket does not make any commitment to update the materials.</p>
+              <p>The materials appearing on EntropiaEstate’s Website may include technical, typographical, or photographic errors. EntropiaEstate will not promise that any of the materials in this Website are accurate, complete, or current. EntropiaEstate may change the materials contained on its Website at any time without notice. EntropiaEstate does not make any commitment to update the materials.</p>
 
               <h2>6. Links</h2>
 
-              <p>EntropiaMarket has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by EntropiaMarket of the site. The use of any linked website is at the user’s own risk.</p>
+              <p>EntropiaEstate has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by EntropiaEstate of the site. The use of any linked website is at the user’s own risk.</p>
 
               <h2>7. Site Terms of Use Modifications</h2>
 
-              <p>EntropiaMarket may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
+              <p>EntropiaEstate may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
 
               <h2>8. Your Privacy</h2>
 
@@ -58,7 +58,7 @@
 
               <h2>9. Governing Law</h2>
 
-              <p>Any claim related to EntropiaMarket's Website shall be governed by the laws of us without regards to its conflict of law provisions.</p>
+              <p>Any claim related to EntropiaEstate's Website shall be governed by the laws of us without regards to its conflict of law provisions.</p>
 
             </div>
           </div>
