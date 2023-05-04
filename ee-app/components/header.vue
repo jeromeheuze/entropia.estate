0a7c79241fa46e4ff83ff17578ae92042ce5eb26
Spectrum3900 <jeromeheuze@gmail.com>
2023-05-03 17:00:33 -0700

    up

--- ee-app/components/header.vue
+++ ee-app/components/header.vue
@@ -1,37 +1,86 @@
 <template>
-  <header id="top">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-lg-12">
-          <nav class="menu-wrapper menu-gray">
-            <ul class="menu">
-              <li><nuxt-link to="/">Home</nuxt-link></li>
-              <li><nuxt-link to="/search-entropia-estates/">Search</nuxt-link></li>
-              <li><nuxt-link to="/guides/">Guides</nuxt-link></li>
-              <li><button role="button" class="btn">Estate Deeds</button>
-                <ul>
-                  <li><nuxt-link to="/deeds-condos-shops-estates/">Estate Deeds 2020</nuxt-link></li>
-                  <li><nuxt-link to="/deeds-condos-shops-estates-2021/">Estate Deeds 2021</nuxt-link></li>
-                  <li><nuxt-link to="/deeds-condos-shops-estates-2022/">Estate Deeds 2022</nuxt-link></li>
-                  <li><nuxt-link to="/deeds-condos-shops-estates-2023/">Estate Deeds 2023</nuxt-link></li>
-                </ul>
-              </li>
-              <li><button role="button" class="btn">Virtual Visits</button>
-                <ul>
-                  <li><nuxt-link to="/arkadia/">Visit Arkadia</nuxt-link></li>
-                  <li><nuxt-link to="/calypso/">Visit Caplyso</nuxt-link></li>
-                  <li><nuxt-link to="/crystal-palace/">Visit Crystal Palace</nuxt-link></li>
-                  <li><nuxt-link to="/foma-asteroid/">Visit Foma Asteroid</nuxt-link></li>
-                  <li><nuxt-link to="/monria/">Visit Monria</nuxt-link></li>
-                  <li><nuxt-link to="/planet-toulan/">Visit Toulan</nuxt-link></li>
-                  <li><nuxt-link to="/next-island/">Visit Next Island</nuxt-link></li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
+    <header id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-lg-12">
+                    <nav class="menu-wrapper menu-gray">
+                        <ul class="menu">
+                            <li>
+                                <nuxt-link to="/">Home</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/search-entropia-estates/">Search</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/guides/">Guides</nuxt-link>
+                            </li>
+                            <li>
+                                <button role="button" class="btn">Estate Deeds</button>
+                                <ul>
+                                    <li>
+                                        <nuxt-link to="/deeds-condos-shops-estates/">Estate Deeds 2020</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/deeds-condos-shops-estates-2021/">Estate Deeds 2021</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/deeds-condos-shops-estates-2022/">Estate Deeds 2022</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/deeds-condos-shops-estates-2023/">Estate Deeds 2023</nuxt-link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <button role="button" class="btn">Virtual Visits</button>
+                                <ul>
+                                    <li>
+                                        <nuxt-link to="/arkadia/">Visit Arkadia</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/calypso/">Visit Caplyso</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/crystal-palace/">Visit Crystal Palace</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/foma-asteroid/">Visit Foma Asteroid</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/monria/">Visit Monria</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/planet-toulan/">Visit Toulan</nuxt-link>
+                                    </li>
+                                    <li>
+                                        <nuxt-link to="/next-island/">Visit Next Island</nuxt-link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <button role="button" class="btn">Other Sites</button>
+                                <ul>
+                                    <li>
+                                        <a href="https://entropiahub.com" target="_blank">Entropia Hub</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://entropia.events" target="_blank">Entropia Events</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://foma-asteroid.com" target="_blank">FOMA Asteroid</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://entropia.university" target="_blank">Entropia University</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
 
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </header>
+    </header>
 </template>
+<script setup>
+</script>
